6f90d570f2afee905d6718a93201829674b403f5
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-06-25 14:16:50 -0500

    tbb: add recipe

---
+++ tbb/2017_20170226/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+make -j${CPU_COUNT}
+
+install -d ${PREFIX}/lib
+# filter libtbb.dylib ( or .so ), libtbbmalloc.dylib ( or .so )
+cp `find . -name "*lib*" | grep tbb | grep release` ${PREFIX}/lib
+
+install -d ${PREFIX}/include
+# copy the include files
+cp -r ./include/tbb ${PREFIX}/include
+
+# simple test instead of "make test" to avoid timeout
+g++ ${RECIPE_DIR}/tbb_example.c -I${PREFIX}/include -L${PREFIX}/lib -ltbb -o tbb_example
+DYLD_LIBRARY_PATH=${PREFIX}/lib ./tbb_example
