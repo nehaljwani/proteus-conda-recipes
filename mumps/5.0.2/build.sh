11895a1b4812a21123f504d8a931908e321cf37b
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-07-09 21:01:31 -0500

    mumps: add recipe

---
+++ mumps/5.0.2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ `uname` == "Darwin" ]; then
+  cp $RECIPE_DIR/Makefile.debian.SEQ_mac Makefile.inc
+else
+  cp $RECIPE_DIR/Makefile.debian.SEQ Makefile.inc
+fi
+
+make CC="${PREFIX}/bin/mpicc" FC="${PREFIX}/bin/mpif90" FL="${PREFIX}/bin/mpif90" all
+cp lib/*.a ${PREFIX}/lib
+cp libseq/*.a ${PREFIX}/lib
+cp libseq/*.h ${PREFIX}/include
+cp include/*.h ${PREFIX}/include
+
+cd examples
+./ssimpletest < input_simpletest_real
+./dsimpletest < input_simpletest_real
+./csimpletest < input_simpletest_cmplx
+./zsimpletest < input_simpletest_cmplx
