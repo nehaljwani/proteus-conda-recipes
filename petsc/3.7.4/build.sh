a8a8d1cab226c48e7f2c1e99d19986b9b98f6c22
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-07-11 11:30:53 -0500

    petsc: fix recipe

--- petsc/3.7.4/build.sh
+++ petsc/3.7.4/build.sh
@@ -15,10 +15,12 @@ $PYTHON ./configure \
   --with-hwloc=0 \
   --with-mpi=1 \
   --with-pthread=1 \
-  --with-ptscotch=1 \
   --with-ssl=0 \
   --with-suitesparse=1 \
   --with-x=0 \
+  --with-parmetis=1 \
+  --with-metis=1 \
+  --with-superlu_dist=1 \
   --prefix=$PREFIX
 
 sedinplace() { [[ $(uname) == Darwin ]] && sed -i "" $@ || sed -i"" $@; }
@@ -35,13 +37,3 @@ if [[ $(uname) == Darwin ]]; then
 fi
 
 make install
-
-rm -fr $PREFIX/bin
-rm -fr $PREFIX/share
-rm -fr $PREFIX/lib/lib$PKG_NAME.*.dylib.dSYM
-rm -f  $PREFIX/lib/$PKG_NAME/conf/files
-rm -f  $PREFIX/lib/$PKG_NAME/conf/*.py
-rm -f  $PREFIX/lib/$PKG_NAME/conf/*.log
-rm -f  $PREFIX/lib/$PKG_NAME/conf/RDict.db
-rm -f  $PREFIX/lib/$PKG_NAME/conf/*BuildInternal.cmake
-find   $PREFIX/include -name '*.html' -delete
