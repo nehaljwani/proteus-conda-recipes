53ff7b3502344c8506fe15cd01f2442e75f78280
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-07-11 15:35:05 -0500

    gmsh: fix recipe

--- gmsh/3.0.3/build.sh
+++ gmsh/3.0.3/build.sh
@@ -3,9 +3,14 @@
 mkdir -p build
 cd build
 
-cmake \
+CC=mpicc CXX=mpicxx cmake \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DENABLE_PETSC:BOOL=ON \
+  -DENABLE_PETSC4PY:BOOL=ON \
+  -DENABLE_FLTK:BOOL=OFF \
+  -DENABLE_BUILD_SHARED:BOOL=ON \
+  -DENABLE_WRAP_PYTHON:BOOL=ON \
   ..
 
 make
